d01c2b66733233875093bbc22e669bee77026cee
atpiggy <andytuwm@gmail.com>
2013-05-29 17:18:28 -0600

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rascarlo_emulator.mk \
+    $(LOCAL_DIR)/rascarlo_crespo.mk \
     $(LOCAL_DIR)/rascarlo_grouper.mk \
     $(LOCAL_DIR)/rascarlo_maguro.mk \
     $(LOCAL_DIR)/rascarlo_maguro_en.mk \
